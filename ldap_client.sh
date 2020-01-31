ca790a513d387c0518f13ff7136049e67f745bef
park215 <55862690+park215@users.noreply.github.com>
2020-01-30 19:31:27 -0800

    Create ldap_client.sh

---
+++ ldap_client.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# note, this is only part of the ldap client install script.  These are the relevent sed lines.
+
+apt-get update
+export DEBIAN_FRONTEND=noninteractive
+apt -y install libnss-ldap libpam-ldap ldap-utils
+unset DEBIAN_FRONTEND
+
+sed -i 's/passwd:         compat systemd/passwd:         compat systemd ldap/g' /etc/nsswitch.conf
+sed -i 's/group:          compat systemd/group:          compat systemd ldap/g' /etc/nsswitch.conf
+sed -i 's/password        \[success=1 user_unknown=ignore default=die\]     pam_ldap.so use_authtok try_first_pass/password        \[success=1 user_unknown=ignore default=die\]     pam_ldap.so try_first_pass/g' /etc/pam.d/common-password
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/base dc=example,dc=net/base dc=nti310,cd=local/g' /etc/ldap.conf
+sed -i 's,uri ldapi:///,uri ldap://ldap,g' /etc/ldap.conf
+sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=ldapadm,dc=nti310,dc=local/g' /etc/ldap.conf
+sed -i 's/nss_base_group          ou=Group,dc=padl,dc=com\?one/nss_base_group          ou=Group,dc=nti310,dc=local/g' /etc/ldap.conf
+sed -i 's/nss_base_passwd ou=People,dc=padl,dc=com\?one/nss_base_passwd ou=People,dc=nti310,dc=local/g' /etc/ldap.conf
+sed -i 's/nss_base_shadow ou=People,dc=padl,dc=com\?one/nss_base_shadow ou=People,dc=nti310,dc=local/g' /etc/ldap.conf
+
+systemctl restart sshd
+echo "XUZZ5g53" > /etc/ldap.secret
+chmod 0600 /etc/ldap.secret
+systemctl restart libnss-ldap
+apt -y install debconf-utils
+
+ldap-auth-config        ldap-auth-config/bindpw password
+ldap-auth-config        ldap-auth-config/rootbindpw     password
+ldap-auth-config        ldap-auth-config/ldapns/ldap-server     string  ldapi:///ldap1
+ldap-auth-config        ldap-auth-config/binddn string  cn=proxyuser,dc=example,dc=net
+ldap-auth-config        ldap-auth-config/pam_password   select  md5
+ldap-auth-config        ldap-auth-config/dbrootlogin    boolean true
+ldap-auth-config        ldap-auth-config/dblogin        boolean false
+ldap-auth-config        ldap-auth-config/rootbinddn     string  cn=ldapdm,dc=nti310,dc=local
+ldap-auth-config        ldap-auth-config/override       boolean true
+ldap-auth-config        ldap-auth-config/ldapns/ldap_version    select  3
+ldap-auth-config        ldap-auth-config/move-to-debconf        boolean true
+ldap-auth-config        ldap-auth-config/ldapns/base-dn string  dc=nti310,dc=local
+
+while read line; do echo "$line" | debconf-set-selections; done < /tmp/ldap_debconf
+
+
