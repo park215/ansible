2f7e3ddb82ada20743aab0e0eb5b4ab104f6fddf
park215 <55862690+park215@users.noreply.github.com>
2020-03-13 12:23:54 -0700

    Update logserv2.sh

--- logserv2.sh
+++ logserv2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "*.info;mail.none;authpriv.none;cron.none   @10.128.15.204" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+echo "*.info;mail.none;authpriv.none;cron.none   @10.128.15.5" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
 #Important: this should be the internal not external IP of the server or the dns name of your server.
