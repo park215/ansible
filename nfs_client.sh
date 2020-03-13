c68dcbfbfe785b302d3c5ed875d7f67e706b56e9
park215 <55862690+park215@users.noreply.github.com>
2020-03-13 12:26:25 -0700

    Update nfs_client.sh

--- nfs_client.sh
+++ nfs_client.sh
@@ -6,5 +6,5 @@ showmount -e 10.128.15.200 #where $ipaddress is the ip of your nfs server
 mkdir /mnt/test
 echo "10.128.15.200:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
 mount -a
-echo "*.info;mail.none;authpriv.none;cron.none   @10.128.15.204" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+echo "*.info;mail.none;authpriv.none;cron.none   @10.128.15.5" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
 # profit 
