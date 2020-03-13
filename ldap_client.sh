07f750efe5974233addb9c31ed06481ed126821a
park215 <55862690+park215@users.noreply.github.com>
2020-03-12 20:16:06 -0700

    Update ldap_client.sh

--- ldap_client.sh
+++ ldap_client.sh
@@ -11,7 +11,7 @@ sed -i 's/group:          compat systemd/group:          compat systemd ldap/g' 
 sed -i 's/password        \[success=1 user_unknown=ignore default=die\]     pam_ldap.so use_authtok try_first_pass/password        \[success=1 user_unknown=ignore default=die\]     pam_ldap.so try_first_pass/g' /etc/pam.d/common-password
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/base dc=example,dc=net/base dc=nti310,cd=local/g' /etc/ldap.conf
-sed -i 's,uri ldapi:///,uri ldap://ldap,g' /etc/ldap.conf
+sed -i 's,uri ldapi:///,uri ldap://client-b,g' /etc/ldap.conf
 sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=ldapadm,dc=nti310,dc=local/g' /etc/ldap.conf
 sed -i "s/#nss_base_group.[ \t]*ou=Group,dc=padl,dc=com?one/nss_base_group          ou=Group,dc=nti310,dc=local/g" /etc/ldap.conf
 sed -i 's/#nss_base_passwd.[ \t]*ou=People,dc=padl,dc=com?one/nss_base_passwd        ou=People,dc=nti310,dc=local/g' /etc/ldap.conf
