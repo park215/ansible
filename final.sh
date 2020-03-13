73b367ec2cebfa7bc5004249ea59fbad81e7ec87
park215 <55862690+park215@users.noreply.github.com>
2020-03-13 12:48:06 -0700

    Update final.sh

--- final.sh
+++ final.sh
@@ -11,28 +11,6 @@ gcloud compute instances create rsyslog-server2 \
 --metadata-from-file startup-script=/home/sxs924/NTI-310/logsrv.sh \
 --private-network-ip=10.128.15.5
 
-#LDAP
-gcloud compute instances create ldap2 \
---image-family centos-7 \
---image-project centos-cloud \
---zone us-central-a \
---tags "http-server","https-server" \
---machine-type f1-micro \
---scopes cloud-platform \
---metadata-from-file startup-script=/home/sxs924/NTI-310/LDAP.sh \
---private-network-ip=10.128.0.6
-
-#nfs
-gcloud compute instances create nfs2 \
---image-family centos-7 \
---image-project centos-cloud \
---zone us-central-a \
---tags "http-server","https-server" \
---machine-type f1-micro \
---scopes cloud-platform \
---metadata-from-file startup-script=/home/sxs924/NTI-310/nfs.sh \
---private-network-ip=10.128.0.7
-
 #postgres
 gcloud compute instances create postgres2 \
 --image-family centos-8 \
@@ -44,6 +22,16 @@ gcloud compute instances create postgres2 \
 --metadata-from-file startup-script=/home/sxs924/NTI-310/postgres.sh \
 --private-network-ip=10.128.0.8
 
+#LDAP
+gcloud compute instances create ldap2 \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-central-a \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform \
+--metadata-from-file startup-script=/home/sxs924/NTI-310/LDAP.sh \
+--private-network-ip=10.128.0.6
 
 #django
 gcloud compute instances create django2 \
@@ -55,6 +43,17 @@ gcloud compute instances create django2 \
 --scopes cloud-platform \
 --metadata-from-file startup-script=/home/sxs924/NTI-310/django.sh \
 --private-network-ip=10.128.0.9
+
+#nfs
+gcloud compute instances create nfs2 \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-central-a \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform \
+--metadata-from-file startup-script=/home/sxs924/NTI-310/nfs.sh \
+--private-network-ip=10.128.0.7
 
 #ubuntu with ldap and nfs
 gcloud compute instances create ubuntu-client \
