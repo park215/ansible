d60c2d26e1563ff3957691a88af6f27b7e13310f
park215 <55862690+park215@users.noreply.github.com>
2020-03-13 12:42:39 -0700

    Update ldap_client.sh

--- ldap_client.sh
+++ ldap_client.sh
@@ -40,8 +40,8 @@ while read line; do echo "$line" | debconf-set-selections; done < /tmp/ldap_debc
 
 apt-get install nfs-client
 
-showmount -e 10.128.15.200 #where $ipaddress is the ip of your nfs server
+showmount -e 10.128.0.7 #where $ipaddress is the ip of your nfs server
 mkdir /mnt/test
-echo "10.128.15.200:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
+echo "10.128.0.7:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
 mount -a
 echo "*.info;mail.none;authpriv.none;cron.none   @10.128.15.5" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
