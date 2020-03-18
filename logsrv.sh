92f148173e0fe22e5aa9c1498bfd8d41a34f3d59
park215 <55862690+park215@users.noreply.github.com>
2020-03-18 14:43:45 -0700

    Update logsrv.sh

--- logsrv.sh
+++ logsrv.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+
+for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
+
+
+echo "[nti-310-epel]
+name=NTI310 EPEL
+baseurl=http://34.71.91.10/epel
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-base]
+name=NTI310 BASE
+baseurl=http://34.71.91.10/base
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-extras]
+name=NTI310 EXTRAS
+baseurl=http://34.71.91.10/extras/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-updates]
+name=NTI310 UPDATES
+baseurl=http://34.71.91.10/updates/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
 sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
 sed -i 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/g' /etc/rsyslog.conf
 sed -i 's/#$ModLoad imtcp/$ModLoad imtcp/g' /etc/rsyslog.conf
