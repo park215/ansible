326baab669b7db2a34702f511f3a9ebbcdb726bb
park215 <55862690+park215@users.noreply.github.com>
2020-03-20 04:11:28 -0700

    Update django.sh

--- django.sh
+++ django.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 #fun django tutorial https://data-flair.training/blogs/django-migrations-and-database-connectivity/
 
-for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
-
-
-echo "[nti-310-epel]
-name=NTI310 EPEL
-baseurl=http://34.71.91.10/epel
-gpgcheck=0
-enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
-echo "[nti-310-base]
-name=NTI310 BASE
-baseurl=http://34.71.91.10/base
-gpgcheck=0
-enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
-echo "[nti-310-extras]
-name=NTI310 EXTRAS
-baseurl=http://34.71.91.10/extras/
-gpgcheck=0
-enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
-echo "[nti-310-updates]
-name=NTI310 UPDATES
-baseurl=http://34.71.91.10/updates/
-gpgcheck=0
-enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
 yum -y install python-pip python-devel gcc postgresql-devel postgresql-contrib
 pip install --upgrade pip
 pip install virtualenv
