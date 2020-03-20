4330092c791d1c456d46d1b6678eae5c6f0bc48b
park215 <55862690+park215@users.noreply.github.com>
2020-03-20 11:54:15 -0700

    Update django.sh

--- django.sh
+++ django.sh
@@ -31,11 +31,12 @@ enabled=1" >> /etc/yum.repos.d/local-repo.repo
 yum -y install python-pip python-devel gcc postgresql-devel postgresql-contrib
 pip install --upgrade pip
 pip install virtualenv
+pip install django psycopg2
+#moved line 38 to 34
 mkdir /opt/nti310
 cd /opt/nti310
 virtualenv nti310env
 source nti310env/bin/activate
-pip install django psycopg2
 django-admin.py startproject nti310 .
 #vim /opt/nti310/nti310/setting.py
 perl -i -0pe "BEGIN{undef $/;} s/        'ENGINE':.*db.sqlite3'\),/        'ENGINE': 'django.db.backends.postgresql_psycopg2',\n        'NAME': 'nti310',\n        'USER': 'nti310user',\n        'PASSWORD': 'password',\n        'HOST': 'postgres2',\n        'PORT': '5432',/smg" /opt/nti310/nti310/settings.py
