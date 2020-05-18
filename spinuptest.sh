cbeba04245917eea29fc26c85ecd4cd84064bd31
park215 <55862690+park215@users.noreply.github.com>
2020-05-17 22:11:31 -0700

    Create spinuptest.sh

---
+++ spinuptest.sh
@@ -0,0 +1,10 @@
+#LDAP
+gcloud compute instances create ldap \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-central1-a \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform \
+--metadata-from-file startup-script=/home/sxs924/NTI-310/LDAP.sh \
+--private-network-ip=10.128.0.6
