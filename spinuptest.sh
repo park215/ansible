9add7e0ba41e75a42b5458ae3b71a5df99a8b31e
park215 <55862690+park215@users.noreply.github.com>
2020-05-18 13:23:05 -0700

    Update spinuptest.sh

--- spinuptest.sh
+++ spinuptest.sh
@@ -1,10 +1,10 @@
-#LDAP
-gcloud compute instances create ldap \
+#django
+gcloud compute instances create django \
 --image-family centos-7 \
 --image-project centos-cloud \
 --zone us-central1-a \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=/home/sxs924/NTI-310/LDAP.sh \
+--metadata-from-file startup-script=/home/sxs924/NTI-310/django.sh \
 --private-network-ip=10.128.0.9
