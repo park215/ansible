b7061d4bbd5b6b13da8ab735759aca5d79c20c42
park215 <55862690+park215@users.noreply.github.com>
2020-05-19 11:52:18 -0700

    Update spinuptest.sh

--- spinuptest.sh
+++ spinuptest.sh
@@ -1,10 +1,10 @@
-#django
-gcloud compute instances create django \
---image-family centos-7 \
+#postgres
+gcloud compute instances create postgres2 \
+--image-family centos-8 \
 --image-project centos-cloud \
 --zone us-central1-a \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=/home/sxs924/NTI-310/django.sh \
---private-network-ip=10.128.0.10
+--metadata-from-file startup-script=/home/sxs924/NTI-310/postgres.sh \
+--private-network-ip=10.128.0.8
